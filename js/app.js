4492f3976807ae44f084098caf5cb73db5a19aee
Aaron <aaroncastro8392@gmail.com>
2024-05-16 22:23:25 -0500

    Show Meals

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 const createOrder = document.querySelector("#guardar-cliente");
+const platillosContainer = document.querySelector("#platillos");
+const consumoContainer = document.querySelector("#resumen");
+
+let client = {
+    table: "",
+    time: "", 
+    order: []
+}
+
+const categorias={
+    1: "Comida",
+    2: "Bebidas",
+    3: "Postres"
+}
 
 createOrder.addEventListener("click",createOrderFunc);
 
@@ -11,6 +25,10 @@ function createOrderFunc(){
         return;
     }
 
+    client.table = table;
+    client.time = timeOrder;
+
+    showContainers()
     hideModal()
 
     extractPlatillos()
@@ -32,10 +50,17 @@ function printError(message, type){
     }, 1500);
 }
 
+function showContainers(){
+    platillosContainer.classList.remove("d-none");
+    consumoContainer.classList.remove("d-none");
+}
+
 function hideModal(){
     const modal = document.querySelector(".modal");
     const modalB = bootstrap.Modal.getInstance(modal)
     modalB.hide()
+
+    
 }
 
 function extractPlatillos(){
@@ -47,8 +72,35 @@ function extractPlatillos(){
 }
 
 function showPlatillos(platillos){
+    const contenido = document.querySelector("#platillos-list")
+    console.log(contenido)
+    console.log(platillos)
+
     platillos.forEach(plato=>{
         const {id, nombre, precio, categoria} = plato;
-        console.log(plato)
+        
+        const row = document.createElement("DIV");
+        row.classList.add("fila-platos")
+        row.id = id
+
+        const mealName =  document.createElement("DIV");
+        const mealPrice = document.createElement("DIV");
+        const mealCategoria = document.createElement("DIV");
+        const quantityDiv = document.createElement("DIV");
+        const quantity = document.createElement("INPUT");
+
+        mealName.textContent = nombre;
+        mealPrice.textContent = precio;
+        mealCategoria.textContent = categorias[categoria];
+        quantity.id - id;
+        quantity.type = "number";
+
+        row.appendChild(mealName);
+        row.appendChild(mealPrice);
+        row.appendChild(mealCategoria);
+        quantityDiv.appendChild(quantity);
+        row.appendChild(quantityDiv);
+
+        contenido.appendChild(row);
     })
 }
